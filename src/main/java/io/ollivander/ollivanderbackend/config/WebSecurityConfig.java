c64f76f33bd9b97157d0dd9d45ae23b1f02814d3
Ta Quoc Hung <hungqtmta2021@gmail.com>
2021-10-29 15:24:01 +0700

    fix

--- src/main/java/io/ollivander/ollivanderbackend/config/WebSecurityConfig.java
+++ src/main/java/io/ollivander/ollivanderbackend/config/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()// avoid CORS and CSRF(Cross Site Request Forgery)
+                .cors().and().csrf().disable()// avoid CORS and CSRF(Cross Site Request Forgery)
                 .httpBasic().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(authEntryPointJwt)

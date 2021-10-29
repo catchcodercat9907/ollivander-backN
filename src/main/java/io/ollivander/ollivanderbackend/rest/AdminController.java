dfab3cfbbd3ab0ed415824fb27ddeb643562a8ef
Ta Quoc Hung <hungqtmta2021@gmail.com>
2021-10-29 15:15:29 +0700

    fix

--- src/main/java/io/ollivander/ollivanderbackend/rest/AdminController.java
+++ src/main/java/io/ollivander/ollivanderbackend/rest/AdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin
+@CrossOrigin(allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "/admin")
 public class AdminController {
